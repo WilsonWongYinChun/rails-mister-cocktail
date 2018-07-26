bf5a9514f7f0e834d8f459389bd33d93cfd56244
YinChunWong <wongyc.wilson@gmail.com>
2018-07-26 12:48:27 +0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.delete_all
+
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+ingredient["drinks"].each_index do |i|
+  item = ingredient["drinks"][i]['strIngredient1']
+  Ingredient.create(name: item)
+end
+
+puts "Created #{Ingredient.count} Ingredients"
