8a83cad83e8c7e1cf135c794eec7b6055f59840c
Winfield Fan <faefaehk@gmail.com>
2018-07-27 17:29:10 +0800

    fuck shit

--- Gemfile
+++ Gemfile
@@ -58,6 +58,9 @@ end
 gem 'dotenv-rails', groups: [:development, :test]
 # photos
 
+gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :show, :new, :create ]do
+  resources :cocktails, only: [ :index, :show, :new, :create]do
     resources :doses, only: [ :new, :create] do
     end
   end
   resources :doses, only: [ :destroy ]
-
+  delete "cocktails/:id", to: "cocktails#destroy", as: :destroy_cocktail
   root to: "cocktails#index"
 end
